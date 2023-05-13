625a6c2565004ce5aa6e061db232643913ac1236
EtiamSani <etiam.sani@laposte.net>
2023-05-13 12:01:26 +0200

    StockTracker page

--- src/components/StockTracker.jsx
+++ src/components/StockTracker.jsx
@@ -10,15 +10,15 @@ const StockTracker = () => {
         <div className=" m-10 ">
           <SearchBar />
         </div>
-        <div>
-          <a className="btn">
+        <div className="flex flex-nowrap items-center justify-center">
+          <a className="btn ">
             Ajouter liste <AiOutlineFileAdd className="mx-1 text-xl" />
           </a>
           <a className="badge-ghost badge badge-sm btn m-2 p-2 font-bold">
             Quality stock
           </a>
         </div>
-        <table className="mx-auto flex table w-96 justify-center">
+        <table className="table-compact max-w-xs  lg:table lg:w-96">
           {/* head */}
           <thead>
             <tr>
@@ -30,6 +30,44 @@ const StockTracker = () => {
           </thead>
           <tbody>
             {/* row 1 */}
+            <tr>
+              <td>
+                <div className="flex items-center space-x-3">
+                  <div className="avatar">
+                    <div className="h-12 w-12 rounded-full">
+                      <img
+                        src="/tailwind-css-component-profile-2@56w.png"
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">Sartorius Stedim</div>
+                    <div className="text-sm opacity-50">DIM</div>
+                  </div>
+                </div>
+              </td>
+              <td className="font-extrabold">
+                298 €
+                <br />
+                <div className="flex text-xs">
+                  <div className="font-bold">+2.4</div>
+                  <div className="w-7 font-bold">+50%</div>
+                </div>
+              </td>
+              <td className="flex justify-center ">
+                200
+                <span className="btn-xs btn ml-2">
+                  <FaPencilAlt className="text-base" />
+                </span>
+              </td>
+              <th>
+                <button className="btn-ghost  btn-sm btn m-0 hover:bg-red-300">
+                  <RiDeleteBin7Line className="text-lg" />
+                </button>
+              </th>
+            </tr>
+            {/* row 2 */}
             <tr>
               <td>
                 <div className="flex items-center space-x-3">
@@ -64,66 +102,49 @@ const StockTracker = () => {
                 </span>
               </td>
               <th>
-                <button className="btn-ghost btn-sm btn hover:bg-red-300">
+                <button className="btn-ghost  btn-sm btn m-0 hover:bg-red-300">
                   <RiDeleteBin7Line className="text-lg" />
                 </button>
               </th>
-            </tr>
-            {/* row 2 */}
-            <tr>
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
-                      <img
-                        src="/tailwind-css-component-profile-3@56w.png"
-                        alt="Avatar Tailwind CSS Component"
-                      />
-                    </div>
-                  </div>
-                  <div>
-                    <div className="font-bold">Brice Swyre</div>
-                    <div className="text-sm opacity-50">China</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Carroll Group
-                <br />
-                <span className="badge-ghost badge badge-sm">
-                  Tax Accountant
-                </span>
-              </td>
-              <td>Red</td>
             </tr>
             {/* row 3 */}
             <tr>
               <td>
                 <div className="flex items-center space-x-3">
                   <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
+                    <div className="h-12 w-12 rounded-full">
                       <img
-                        src="/tailwind-css-component-profile-4@56w.png"
+                        src="/tailwind-css-component-profile-2@56w.png"
                         alt="Avatar Tailwind CSS Component"
                       />
                     </div>
                   </div>
                   <div>
-                    <div className="font-bold">Marjy Ferencz</div>
-                    <div className="text-sm opacity-50">Russia</div>
+                    <div className="font-bold">Sartorius Stedim</div>
+                    <div className="text-sm opacity-50">DIM</div>
                   </div>
                 </div>
               </td>
-              <td>
-                Rowe-Schoen
+              <td className="font-extrabold">
+                298 €
                 <br />
-                <span className="badge-ghost badge badge-sm">
-                  Office Assistant I
+                <span className="badge-ghost badge badge-sm font-bold">
+                  +2.4
+                </span>
+                <span className="badge-ghost badge badge-sm font-bold">
+                  +5 %
                 </span>
               </td>
-              <td>Crimson</td>
+              <td>
+                200 €
+                <span className="btn-xs btn ml-2">
+                  <FaPencilAlt className="text-base" />
+                </span>
+              </td>
               <th>
-                <button className="btn-ghost btn-xs btn">details</button>
+                <button className="btn-ghost  btn-sm btn m-0 hover:bg-red-300">
+                  <RiDeleteBin7Line className="text-lg" />
+                </button>
               </th>
             </tr>
             {/* row 4 */}
@@ -131,29 +152,39 @@ const StockTracker = () => {
               <td>
                 <div className="flex items-center space-x-3">
                   <div className="avatar">
-                    <div className="mask mask-squircle h-12 w-12">
+                    <div className="h-12 w-12 rounded-full">
                       <img
-                        src="/tailwind-css-component-profile-5@56w.png"
+                        src="/tailwind-css-component-profile-2@56w.png"
                         alt="Avatar Tailwind CSS Component"
                       />
                     </div>
                   </div>
                   <div>
-                    <div className="font-bold">Yancy Tear</div>
-                    <div className="text-sm opacity-50">Brazil</div>
+                    <div className="font-bold">Sartorius Stedim</div>
+                    <div className="text-sm opacity-50">DIM</div>
                   </div>
                 </div>
               </td>
-              <td>
-                Wyman-Ledner
+              <td className="font-extrabold">
+                298 €
                 <br />
-                <span className="badge-ghost badge badge-sm">
-                  Community Outreach Specialist
+                <span className="badge-ghost badge badge-sm font-bold">
+                  +2.4
+                </span>
+                <span className="badge-ghost badge badge-sm font-bold">
+                  +5 %
                 </span>
               </td>
-              <td>Indigo</td>
+              <td>
+                200 €
+                <span className="btn-xs btn ml-2">
+                  <FaPencilAlt className="text-base" />
+                </span>
+              </td>
               <th>
-                <button className="btn-ghost btn-xs btn">details</button>
+                <button className="btn-ghost  btn-sm btn m-0 hover:bg-red-300">
+                  <RiDeleteBin7Line className="text-lg" />
+                </button>
               </th>
             </tr>
           </tbody>
