ec68423f29c88686e4883f36a4c2e8e28124db0c
EtiamSani <etiam.sani@laposte.net>
2023-06-05 13:24:38 +0200

    Add testimonials

---
+++ src/APIServices/testimonialsData.js
@@ -0,0 +1,24 @@
+export const testimonialsData = [
+  {
+    id: 1,
+    name: "John Doe",
+    comment: "Amazing experience! The team delivered beyond my expectations.",
+    rating: 5,
+    avatar: "../../images/john.jpeg",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    comment:
+      "I'm impressed with their innovative solutions. Highly recommended.",
+    rating: 4,
+    avatar: "../../images/jane.jpeg",
+  },
+  {
+    id: 3,
+    name: "Alex Johnson",
+    comment: "Beautiful work! The attention to detail is remarkable.",
+    rating: 5,
+    avatar: "../../images/alex.jpeg",
+  },
+];
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,4 +1,18 @@
+import { testimonialsData } from "../APIServices/testimonialsData";
+
 const Testimonial = () => {
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+      stars.push(
+        <span key={i} className="mr-1 text-yellow-500">
+          &#9733;
+        </span>
+      );
+    }
+    return stars;
+  };
+
   return (
     <div>
       <h2 className="mb-4 text-center text-3xl font-bold sm:text-5xl">
@@ -7,6 +21,28 @@ const Testimonial = () => {
       <p className="mb-6 text-center text-lg sm:text-2xl md:mb-14">
         We create engaging experiences that are innovative and beautiful.
       </p>
+
+      <div className="flex flex-wrap justify-center">
+        {testimonialsData.map((testimonial) => (
+          <div
+            key={testimonial.id}
+            className="mx-2 mb-6 max-w-md rounded-lg bg-white p-4 shadow-lg"
+          >
+            <div className="mb-4 flex items-center">
+              <img
+                src={testimonial.avatar}
+                alt={testimonial.name}
+                className="mr-2 h-10 w-10 rounded-full"
+              />
+              <h3 className="mb-2 text-lg font-bold">{testimonial.name}</h3>
+            </div>
+            <p className="text-gray-700">{testimonial.comment}</p>
+            <div className="mt-4 flex items-center">
+              {renderStars(testimonial.rating)}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
