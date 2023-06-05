6cd12df08f4f07544599bce854688a1a071d2dfe
EtiamSani <etiam.sani@laposte.net>
2023-06-05 15:56:30 +0200

    Style homepage

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,4 +1,4 @@
-import gif from "../styles/App2.gif";
+import mockup from "../images/Mockup.png";
 
 const Presentation = () => {
   return (
@@ -6,9 +6,9 @@ const Presentation = () => {
       <h1 className=" mt-10 text-center text-3xl font-bold">
         Lorem ipsum dolor sit amet.
       </h1>
-      <div className="mockup-window mx-48 my-8 border bg-base-300">
-        <div className="flex justify-center bg-base-200 px-4 py-16">
-          <img src={gif} alt="GIF" />
+      <div className="mx-48">
+        <div className="flex justify-center px-4 ">
+          <img src={mockup} className="w-50" alt="Mockup" />
         </div>
       </div>
     </div>
