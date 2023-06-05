b034e940fd5436fc6c2d51dd223ee44233f36508
EtiamSani <etiam.sani@laposte.net>
2023-06-05 15:59:13 +0200

    Style homepage

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -3,7 +3,7 @@ import mockup from "../images/Mockup.png";
 const Presentation = () => {
   return (
     <div>
-      <h1 className=" mt-10 text-center text-3xl font-bold">
+      <h1 className=" mt-10 text-center text-5xl font-bold">
         Lorem ipsum dolor sit amet.
       </h1>
       <div className="mx-48">
