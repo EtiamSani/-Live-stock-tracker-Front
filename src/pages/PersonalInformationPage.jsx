970f81141bdad23bab26225e66301d5f1be31acf
EtiamSani <etiam.sani@laposte.net>
2023-06-08 18:16:00 +0200

    Add alert in details page

--- src/pages/PersonalInformationPage.jsx
+++ src/pages/PersonalInformationPage.jsx
@@ -154,9 +154,25 @@ const PersonalInformationPage = () => {
               />
             </div>
             {isUpdated && (
-              <p className="text-green-500">
-                Informations mises à jour avec succès
-              </p>
+              <div className="h-30 alert alert-success m-5  w-72">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 shrink-0 stroke-current"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span className="text-center">
+                  {" "}
+                  Informations mises à jour avec succès
+                </span>
+              </div>
             )}
             <button
               type="submit"
