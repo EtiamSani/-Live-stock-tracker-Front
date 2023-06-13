5a896f5438235a661fa398711daa41f3284a26f2
EtiamSani <etiam.sani@laposte.net>
2023-06-13 18:29:44 +0200

    Hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,16 @@ const Hero = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">
+            Lorem ipsum dolor sit amet.
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            voluptatem nostrum accusamus ipsum sint ut officia, veniam soluta
+            aliquid totam.
           </p>
           <Link to="/auth/register-investor">
-            <button className="btn-primary btn">Get Started</button>
+            <button className="btn-primary btn">Commencer ! </button>
           </Link>
         </div>
       </div>
