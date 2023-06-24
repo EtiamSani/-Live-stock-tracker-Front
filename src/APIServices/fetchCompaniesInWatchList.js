b8c7c69f62ecc6d3c811c124f60439fcbb57fe7e
EtiamSani <etiam.sani@laposte.net>
2023-06-24 11:15:07 +0200

    Fix bugs

--- src/APIServices/fetchCompaniesInWatchList.js
+++ src/APIServices/fetchCompaniesInWatchList.js
@@ -1,15 +1,20 @@
 const fetchCompaniesInWatchLists = async () => {
   const selectedId = localStorage.getItem("selectedId");
 
-  const apiRes = await fetch(
-    `https://stock-tracker-api.up.railway.app/watchlist/${selectedId}`
-  );
+  if (selectedId) {
+    const apiRes = await fetch(
+      `https://stock-tracker-api.up.railway.app/watchlist/${selectedId}`
+    );
 
-  if (!apiRes.ok) {
-    throw new Error("fetch not ok");
+    if (!apiRes.ok) {
+      throw new Error("fetch not ok");
+    }
+
+    return apiRes.json();
+  } else {
+    // Traitez le cas où selectedId est null ou undefined
+    throw new Error("Créez ou sélectionnez une liste");
   }
-
-  return apiRes.json();
 };
 
 export default fetchCompaniesInWatchLists;
